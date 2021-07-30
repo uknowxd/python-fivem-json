a27d073398e930b9e63d47f154b0b0ad8471a097
uknowxd <kitisaksrisuk@gmail.com>
2021-07-30 18:13:44 +0700

    Add Input IP Server

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
-import json
-
+import time
 # ดึงค่า API Json จาก เป้าหมาย
-response = requests.get('http://103.167.193.24:30120/players.json')
+ip_user = input('IP Server : ')
+response = requests.get('http://'+ip_user+':30120/players.json')
 data = response.json() 
 
+
 # ทำการแสดงค่าต่างๆที่ดึงมา ตัวอย่าง ชื่อผู้เล่น กับ Ping ของผู้เล่น
-for get_data in data:
-     print (('ชื่อผู้เล่น :'),get_data['name'] , (' ||||| ') ,('Ping :'),get_data['ping'])
+while True:
+     for get_data in data:
+          print (('ชื่อผู้เล่น :'),get_data['name'] , (' ||||| ') ,('Ping :'),get_data['ping'])
+          print('=============================================')
+     time.sleep(5)
